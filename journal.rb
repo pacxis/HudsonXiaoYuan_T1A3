43ce9b41247d046c51abc55d212c35d89359fcd8
pacxis <yuan_hudson@hotmail.com>
2022-03-26 17:25:33 +1000

    added-first-test

---
+++ journal.rb
@@ -0,0 +1,8 @@
+case ARGV[0]
+when "-n"
+    return "hello"
+end
+
+
+# file = File.open("#{title}.txt", 'w')
+# file.close
--- spec/journal_spec.rb
+++ spec/journal_spec.rb
@@ -1,1 +1,10 @@
 require './journal.rb'
+
+describe ARGV[0] do
+    context 'given -n' do
+        let(ARGV[0]) == 
+        it 'returns hello' do
+            expect('hello')
+        end
+    end
+end
