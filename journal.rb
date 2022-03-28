795878353e7c96483ff9871472cd1d2e61977ed5
pacxis <yuan_hudson@hotmail.com>
2022-03-28 16:24:39 +1000

    added-feelings-class

--- journal.rb
+++ journal.rb
@@ -11,14 +11,19 @@ class InvalidOptionError < StandardError
     end
 end
 
+class InvalidFeeling < StandardError
+    def message
+        return "Invalid entry, feeling must be one of the following: #{entry_categories.feelings.join(', ')}"
+    end
+end
+
 class Menu
-    attr_reader :menu_args
-    
+
     def initialize(*menu_args)
         @menu_args = menu_args
     end
 
-    #Prints menu
+    #Prints menu with index
     def display_menu
         puts "Please select an option (1 to #{@menu_args.length})"
         puts "Enter \"help\" to see options again"
@@ -35,11 +40,26 @@ class Menu
     end
 end
 
+class Emotions
+    attr_reader :feelings
 
+    def initialize(*feelings)
+        @feelings = feelings
+    end
+end
 
 main_menu = Menu.new("New Journal Entry", "View All Journal Entries", "Search Journal Entries", "Exit")
 
-# puts "enter \"help\" to see options"
+entry_categories = Emotions.new("okay, happy, sad, angry, stressed, anxious, suprised, confused")
+
+def get_feeling
+    puts "What is your overall feeling today? #{entry_categories.feelings.join(', ')}"
+    input = $stdin.gets.strip.downcase
+    unless entry_categories.feelings.include?(input)
+        raise(InvalidFeeling)
+    end
+    return input
+end
 
 # Displays error message if menu selection is invalid
 begin
@@ -52,14 +72,30 @@ case ARGV[0]
 when nil, "help"
     main_menu.display_menu
 when "1"
-    date_time = DateTime.now
-
+    date_time = DateTime.now.strftime("%d/%m/%Y %H:%M")
     id = UUIDTools::UUID.timestamp_create.to_s
     file = File.new("#{id}.txt", 'w')
     FileUtils.move("#{id}.txt", "Entries/#{id}.txt")
     puts "Please enter the title: "
+    # Adding title to entry
     file << $stdin.gets.strip + "\n"*2
-    file << date_time.strftime("%d/%m/%Y %H:%M") + "\n"*2
+    # Adding date and time to entry
+    file << date_time + "\n"*2
+    # puts "What is your overall feeling today? #{entry_categories.feelings.join(', ')}"
+    begin
+        # feeling = @stdin.gets.strip.downcase
+        # unless entry_categories.feelings.include?(feeling)
+        #     raise(InvalidFeeling)
+        feeling = get_feeling
+    rescue InvalidFeeling => e
+            puts e.message
+            retry
+        puts "feelings successful"
+        
+    end
+
+    
+    
 when "2"
     puts "view all"
 when "3"
