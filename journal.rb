de1ae62fd4b854347d1b5cac9425e589e4eb5789
pacxis <yuan_hudson@hotmail.com>
2022-03-29 13:52:50 +1000

    added-intensity

--- journal.rb
+++ journal.rb
@@ -1,45 +1,35 @@
 
-# file = File.open("#{title}.txt", 'w')
-# file.close
 require 'date'
 require 'uuidtools'
 require 'fileutils'
 
-class InvalidOptionError < StandardError
-    def message
-        return "Invalid selection, try again"
-    end
-end
+# -----------------------OTHER CLASSES------------------
 
-class InvalidFeeling < StandardError
-    def message
-        return "Invalid entry, feeling must be one of the following: #{entry_categories.feelings.join(', ')}"
-    end
-end
-
+# Class for menus
 class Menu
 
-    def initialize(*menu_args)
-        @menu_args = menu_args
+    def initialize(*menu_items)
+        @menu_items = menu_items
     end
 
-    #Prints menu with index
+    # Prints menu with index
     def display_menu
-        puts "Please select an option (1 to #{@menu_args.length})"
+        puts "Please select an option (1 to #{@menu_items.length})"
         puts "Enter \"help\" to see options again"
-        @menu_args.each_with_index do |item, index|
+        @menu_items.each_with_index do |item, index|
             puts "#{index + 1}. #{item}"
         end
     end
 
     def input_check
         input = ARGV[0]
-        unless input.to_i.between?(0, @menu_args.length) == true || input == "help"
+        unless input.to_i.between?(0, @menu_items.length) == true || input == "help"
             raise InvalidOptionError
         end
     end
 end
 
+# Class for emotions
 class Emotions
     attr_reader :feelings
 
@@ -48,18 +38,53 @@ class Emotions
     end
 end
 
+#------------------CLASS INSTANTIATION--------------------
+
 main_menu = Menu.new("New Journal Entry", "View All Journal Entries", "Search Journal Entries", "Exit")
 
-entry_categories = Emotions.new("okay, happy, sad, angry, stressed, anxious, suprised, confused")
+entry_categories = Emotions.new("okay", "happy", "sad", "angry", "stressed", "anxious", "suprised", "confused")
 
-def get_feeling
-    puts "What is your overall feeling today? #{entry_categories.feelings.join(', ')}"
-    input = $stdin.gets.strip.downcase
-    unless entry_categories.feelings.include?(input)
+# ----------------ERROR CLASSES---------------------------
+
+class InvalidOptionError < StandardError
+    def message
+        return "Invalid menu selection, try again"
+    end
+end
+
+class InvalidFeeling < StandardError
+    def message(feel)
+        return "Invalid feeling entry, input one of the following: #{feel.feelings.join(', ')}"
+    end
+end
+
+class InvalidIntensity < StandardError
+    def message
+        return "Invalid intensity entry, input a number from 1 to 5"
+    end
+end
+
+# ----------------------METHODS----------------------------
+
+def get_feeling(feel)
+    input = gets.strip.downcase
+    if feel.feelings.include?(input)
+        return input
+    else
         raise(InvalidFeeling)
     end
-    return input
 end
+
+def get_intensity
+    input = gets.strip.to_i
+    if input.between?(1, 5)
+        return input
+    else
+        raise(InvalidIntensity)
+    end
+end
+
+#-------------------------MAIN CODE-------------------------
 
 # Displays error message if menu selection is invalid
 begin
@@ -72,29 +97,38 @@ case ARGV[0]
 when nil, "help"
     main_menu.display_menu
 when "1"
+    ARGV.clear
+    # Generating current date and time
     date_time = DateTime.now.strftime("%d/%m/%Y %H:%M")
+    # Generating unique ID for entry
     id = UUIDTools::UUID.timestamp_create.to_s
-    file = File.new("#{id}.txt", 'w')
-    FileUtils.move("#{id}.txt", "Entries/#{id}.txt")
+    # Moving entry to Entries folder
     puts "Please enter the title: "
-    # Adding title to entry
-    file << $stdin.gets.strip + "\n"*2
+    # Getting title input from user
+    title = gets.strip
     # Adding date and time to entry
-    file << date_time + "\n"*2
-    # puts "What is your overall feeling today? #{entry_categories.feelings.join(', ')}"
+    puts "What is your overall feeling today? (#{entry_categories.feelings.join(', ')})"
     begin
-        # feeling = @stdin.gets.strip.downcase
-        # unless entry_categories.feelings.include?(feeling)
-        #     raise(InvalidFeeling)
-        feeling = get_feeling
+        feeling = get_feeling(entry_categories)
     rescue InvalidFeeling => e
-            puts e.message
+            puts e.message(entry_categories)
             retry
-        puts "feelings successful"
-        
     end
+    if feeling != "okay"
+        puts "How would you rank the intensity of this feeling from 1 to 5? (1 being the weakest)"
+    end
+    begin
+    # intensity = @stdin.gets.strip.to_i
+    intensity = get_intensity
+    rescue InvalidIntensity => e
+        puts e.message
+        retry
+    end
+    puts "Type out your journal entry: "
+    entry = gets.strip
+    file = File.new("#{id}.txt", 'w')
 
-    
+    # FileUtils.move("#{id}.txt", "Entries/#{id}.txt")
     
 when "2"
     puts "view all"
--- p.rb
+++ p.rb
@@ -1,35 +1,3 @@
-# class Menu
-#     attr_reader :menu_args
-    
-#     def initialize(*menu_args)
-#         @menu_args = menu_args
-#     end
-
-#     #Prints menu
-#     def display_menu
-#         puts "Please select an option (1 to #{@menu_args.length})"
-#         puts "Enter \"help\" to see options again"
-#         @menu_args.each_with_index do |item, index|
-#             puts "#{index + 1}. #{item}"
-#         end
-#     end
-
-#     def input_check
-#         input = ARGV[0]
-#         unless input.to_i.between?(0, @menu_args.length) == true || input == "help"
-#             raise InvalidOptionError
-#         end
-#     end
-# end
-
-# main_menu = Menu.new("New Journal Entry", "View All Journal Entries", "Search Journal Entries", "Exit")
-
-# puts main_menu.menu_args do
-
-a = "hello"
-
-def poo
-    puts a
-end
 
 
+TTY::Editor.open("hi.txt")
