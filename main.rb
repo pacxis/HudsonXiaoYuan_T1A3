f93249f69a46bb5d1df865abc1eb3945a1e12ca7
pacxis <yuan_hudson@hotmail.com>
2022-04-01 14:38:13 +1000

    finished-first-feature

--- main.rb
+++ main.rb
@@ -125,69 +125,34 @@ while selection != "Exit" do
         else
             intensity = 0
         end
-        puts "Type out your journal entry: "
-        entry = gets.strip
-        puts "Do you want to save this entry? (y/n)"
+        entry = prompt.ask("Type out your journal entry: ")
+        
+        selection = prompt.yes?("Do you want to save this entry?")
+    
+        if selection == true
+            file = File.open("#{id}.txt", 'w')
+            FileUtils.mv("#{id}.txt", "Entries/#{id}.txt")
+            file << title + "\n"*2
+            file << date + "\n"*2
+            file << feeling + " (#{intensity})" + "\n"*2
+            file << entry
 
-        begin
-            selection = gets.strip
-            case selection
-            when "y"
-                puts "Journal entry saved"
 
-                # Writing saved information to txt file
-                file = File.open("#{id}.txt", 'w')
-                FileUtils.mv("#{id}.txt", "Entries/#{id}.txt")
-                file << title + "\n"*2
-                file << date + "\n"*2
-                file << feeling + " (#{intensity})" + "\n"*2
-                file << entry
-
-                # Writing entry info to json file
-                entry_info = { title: title, id: id, date: date, feeling: feeling, intensity: intensity }
-                File.open('journal_index.json', 'w') do |f|
-                    f.puts JSON.pretty_generate(j_index << entry_info)
-                end
-            when "n"
-                puts "Journal entry discarded"
-            else
-                raise InvalidYesNo
+            entry_info = { title: title, id: id, date: date, feeling: feeling, intensity: intensity }
+            File.open('journal_index.json', 'w') do |f|
+                f.puts JSON.pretty_generate(j_index << entry_info)
             end
-        rescue InvalidYesNo => e
-            puts e.message
-            retry
+             puts "Journal entry saved"
+        else 
+            puts "Journal entry discarded"
         end
     when main_menu.menu_items[1]
-        puts Date.today
-        puts Date.today.strftime("%d/%m/%Y")
-
-        file = File.open("#{id}.txt", 'w')
-        FileUtils.mv("#{id}.txt", "Entries/#{id}.txt")
-        file << title + "\n"*2
-        file << date_time + + "\n"*2
-        file << feeling + " (#{intensity})" + "\n"*2
-        file << entry
-        
+        prompt.ask()
     when main_menu.menu_items[2]
-        date = rand(1..30).to_s + '/' + rand(1..12).to_s + '/' + rand(2020..2021).to_s
-        title = "ran into my friend today"
-        feeling = "suprised"
-        intensity = rand(1..5)
-        id = UUIDTools::UUID.timestamp_create.to_s
-        entry = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam cursus augue sit amet tincidunt ornare. Donec nec venenatis nisl, rutrum scelerisque nibh. Sed a tincidunt felis. Praesent rutrum semper fringilla. Donec hendrerit nulla sed ultrices efficitur. Nullam sit amet molestie nibh. Quisque eget ornare lectus. Proin in accumsan lectus. Pellentesque lectus orci, accumsan id diam et, volutpat aliquam lorem. Integer consequat faucibus ullamcorper. Nulla interdum accumsan quam, sit amet condimentum erat blandit ut."
-        file = File.open("#{id}.txt", 'w')
-        FileUtils.mv("#{id}.txt", "Entries/#{id}.txt")
-        file << title + "\n"*2
-        file << date + "\n"*2
-        file << feeling + " (#{intensity})" + "\n"*2
-        file << entry
-        entry_info = { title: title, id: id, date: date, feeling: feeling, intensity: intensity }
-        File.open('journal_index.json', 'w') do |f|
-            f.puts JSON.pretty_generate(j_index << entry_info)
-        end
-        file.close
+        puts "hello"
     end
 end
+
 
 
 
