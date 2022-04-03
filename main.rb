441cc9070daf402252003330180b3c6e8b0ec1ef
pacxis <yuan_hudson@hotmail.com>
2022-04-03 10:06:01 +1000

    added-search-menu

--- main.rb
+++ main.rb
@@ -7,9 +7,8 @@ require 'tty-prompt'
 require 'awesome_print'
 require 'colorize'
 
-require_relative 'classes.rb'
-require_relative 'methods.rb'
-
+require_relative 'classes'
+require_relative 'methods'
 
 main_menu = Menu.new("New Journal Entry", "View All Journal Entries", "Search Journal Entries", "Exit")
 
@@ -19,6 +18,17 @@ view_menu = Menu.new("By date", "By feeling", "By title alphabetical order", "Re
 
 view_menu_after = Menu.new("View another entry", "Return to previous menu")
 
+search_menu = Menu.new(
+    "Enter date parameters",
+    "Enter title parameters",
+    "Enter feeling parameters",
+    "Enter intensity parameters",
+    "Begin search",
+    "Return to main menu"
+)
+     
+    
+
 
 j_index = JSON.load_file('journal_index.json', symbolize_names: true)
 prompt = TTY::Prompt.new
@@ -27,16 +37,12 @@ selection = nil
 while selection != main_menu.menu_items[-1]
     system "clear"
     selection = ARGV[0].to_s
-    if selection == ""
-        selection = prompt.select("What would you like to do?", main_menu.menu_items)
-    end
+    selection = prompt.select("What would you like to do?", main_menu.menu_items) if selection == ""
+
     case selection
     when main_menu.menu_items[0], '-n'
 
         date = Date.today.strftime("%d/%m/%Y")
-        # month = Date.today.strftime("%m")
-        # day = Date.today.strftime("%d")
-        # date = "#{day}/#{month}/#{year}"
         id = UUIDTools::UUID.timestamp_create.to_s
         title = prompt.ask("Please enter the title: ")
         feeling = prompt.select("What is your overall feeling today?", entry_categories.feelings)
@@ -82,9 +88,9 @@ while selection != main_menu.menu_items[-1]
             puts e.message
         end
 
-        while selection != view_menu.menu_items[-1] do
+        while selection != view_menu.menu_items[-1]
             system "clear"
-            selection = prompt.select("How would you like to view your entries?", view_menu.menu_items)
+            selection = prompt.select("How would you like to view your journal entries?", view_menu.menu_items)
 
             case selection
             when view_menu.menu_items[0]
@@ -98,7 +104,7 @@ while selection != main_menu.menu_items[-1]
                 end
 
             when view_menu.menu_items[1]
-                while selection != view_menu_after.menu_items[-1] do
+                while selection != view_menu_after.menu_items[-1]
                     sorted_entries = j_index.sort { |a, b| [a[:feeling], a[:intensity]] <=> [b[:feeling], b[:intensity]] }
                     sorted_entries.each_with_index do |hash, index|
                         puts "\nJournal Entry #{index + 1}".underline
@@ -108,8 +114,8 @@ while selection != main_menu.menu_items[-1]
                 end
 
             when view_menu.menu_items[2]
-                while selection != view_menu_after.menu_items[-1] do
-                    sorted_entries = j_index.sort_by { |key| key[:title] }
+                while selection != view_menu_after.menu_items[-1]
+                    sorted_entries = j_index.sort_by { |h| h[:title] }
                     sorted_entries.each_with_index do |hash, index|
                         puts "\nJournal Entry #{index + 1}".underline
                         ap hash.slice(:title, :feeling, :intensity, :date)
@@ -120,50 +126,23 @@ while selection != main_menu.menu_items[-1]
         end
 
     when main_menu.menu_items[2], '-s'
-        # begin
-        #     raise NoEntriesError if j_index.empty?
-        # rescue NoEntriesError => e
-        #     puts e.message
-        # end
-        
-
-
-
-
-day = rand(1..30)
-if day.between?(1, 9)
-    day = '0' + day.to_s
-end
-month = rand(1..12)
-if month.between?(1, 9)
-    month = '0' + month.to_s
-end
-year = rand(2020..2021).to_s
-date = "#{day}/#{month}/#{year}"
-feel = ["happy", "sad", "anxious", "stressed", "suprised", "confused", "angry"]
-fruits = ["apple", "pear", "raspberry", "blueberry", "lemon", "cherry", "orange", "grapefruit", "kiwifruit", "tomato", "watermelon", "papaya", "pineapple", "strawberry", "persimmon", "plum", "peach", "mandarin", "tangerine", "banana", "coconut", "apricot", "cranberry", "grapes", "guava", "lychee", "mango", "passion fruit"]
-        title = fruits.sample
-        # feeling = feel.sample
-        feeling = "okay"
-        # intensity = rand(1..5)
-intensity = 0
-        id = UUIDTools::UUID.timestamp_create.to_s
-        entry = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam cursus augue sit amet tincidunt ornare. Donec nec venenatis nisl, rutrum scelerisque nibh. Sed a tincidunt felis. Praesent rutrum semper fringilla. Donec hendrerit nulla sed ultrices efficitur. Nullam sit amet molestie nibh. Quisque eget ornare lectus. Proin in accumsan lectus. Pellentesque lectus orci, accumsan id diam et, volutpat aliquam lorem. Integer consequat faucibus ullamcorper. Nulla interdum accumsan quam, sit amet condimentum erat blandit ut."
-        file = File.open("#{id}.txt", 'w')
-        FileUtils.mv("#{id}.txt", "Entries/#{id}.txt")
-        file << title + "\n"*2
-        file << date + "\n"*2
-        file << feeling + " (#{intensity})" + "\n"*2
-        file << entry
-        entry_info = { title: title, id: id, date: date, feeling: feeling, intensity: intensity }
-        File.open('journal_index.json', 'w') do |f|
-            f.puts JSON.pretty_generate(j_index << entry_info)
+        begin
+            raise NoEntriesError if j_index.empty?
+        rescue NoEntriesError => e
+            puts e.message
         end
-        file.close
-        exit
 
+        s_selection = ""
+        while s_selection != search_menu.menu_items[-1]
+            s_selection = prompt.select("How would you like to search your journal entries?", search_menu.menu_items)
+            case s_selection
+            when search_menu.menu_items[0]
+                
+            when search_menu.menu_items[1]
+                
+            when search_menu.menu_items[2]
 
-
+            when search_menu.menu_items[3]
 
     when main_menu.menu_items[3]
         exit
@@ -175,6 +154,7 @@ intensity = 0
         end
         exit
     end
+    exit unless ARGV[0].nil?
 end
 
 
--- p.rb
+++ p.rb
@@ -1,11 +1,20 @@
-day = rand(1..30).to_s
-month = rand(1..12).to_s
+day = rand(1..30)
+if day.between?(1, 9)
+    day = '0' + day.to_s
+end
+month = rand(1..12)
+if month.between?(1, 9)
+    month = '0' + month.to_s
+end
 year = rand(2020..2021).to_s
 date = "#{day}/#{month}/#{year}"
-        title = "unsure what happened today"
-        feeling = "unsure"
-        intensity = rand(1..5)
-# intensity = 0
+feel = ["happy", "sad", "anxious", "stressed", "suprised", "confused", "angry"]
+fruits = ["apple", "pear", "raspberry", "blueberry", "lemon", "cherry", "orange", "grapefruit", "kiwifruit", "tomato", "watermelon", "papaya", "pineapple", "strawberry", "persimmon", "plum", "peach", "mandarin", "tangerine", "banana", "coconut", "apricot", "cranberry", "grapes", "guava", "lychee", "mango", "passion fruit"]
+        title = fruits.sample
+        # feeling = feel.sample
+        feeling = "okay"
+        # intensity = rand(1..5)
+intensity = 0
         id = UUIDTools::UUID.timestamp_create.to_s
         entry = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam cursus augue sit amet tincidunt ornare. Donec nec venenatis nisl, rutrum scelerisque nibh. Sed a tincidunt felis. Praesent rutrum semper fringilla. Donec hendrerit nulla sed ultrices efficitur. Nullam sit amet molestie nibh. Quisque eget ornare lectus. Proin in accumsan lectus. Pellentesque lectus orci, accumsan id diam et, volutpat aliquam lorem. Integer consequat faucibus ullamcorper. Nulla interdum accumsan quam, sit amet condimentum erat blandit ut."
         file = File.open("#{id}.txt", 'w')
@@ -14,8 +23,9 @@ date = "#{day}/#{month}/#{year}"
         file << date + "\n"*2
         file << feeling + " (#{intensity})" + "\n"*2
         file << entry
-        entry_info = { title: title, id: id, feeling: feeling, intensity: intensity, year: year.to_i, month: month.to_i, day: day.to_i }
+        entry_info = { title: title, id: id, date: date, feeling: feeling, intensity: intensity }
         File.open('journal_index.json', 'w') do |f|
             f.puts JSON.pretty_generate(j_index << entry_info)
         end
         file.close
+        exit
