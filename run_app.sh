6431c6b8453b525feaaf6c030f4d5ae5574728a5
pacxis <yuan_hudson@hotmail.com>
2022-04-06 17:52:46 +1000

    fixed_runapp

--- run_app.sh
+++ run_app.sh
@@ -10,21 +10,21 @@ printf "    Enter '-v' to view journal entries\n";
 printf "    Enter '-s' to search journal entries\n";
 printf "    Enter '-r' to reset journal\n";
 printf "    Any other entry will open the main menu\n";
-read selection
+read -r selection
 
-
-if [ "-n" == $selection ];
-then 
-    ruby main.rb -n
-elif [ "-v" == $selection ]
-then
-    ruby main.rb -v
-elif [ "-s" == $selection ]
-then
-    ruby main.rb -s
-elif [ "-r" == $selection ]
-then
-    ruby main.rb -r
-else
-    ruby main.rb
-fi
+case $selection in
+    -n)
+        ruby main.rb -n
+        ;;
+    -v)
+        ruby main.rb -v
+        ;;
+    -s)
+        ruby main.rb -s
+        ;;
+    -r)
+        ruby main.rb -r
+        ;;
+    *)
+        ruby main.rb
+esac
